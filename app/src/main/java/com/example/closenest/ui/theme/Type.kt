c1b4d64e85095b2782de07b290f89eab3eb9f390
dgb1106 <duonggiabao1106@gmail.com>
2026-04-28 17:52:45 +0700

    add font certs

--- app/src/main/java/com/example/closenest/ui/theme/Type.kt
+++ app/src/main/java/com/example/closenest/ui/theme/Type.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.unit.sp
 
 import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.text.googlefonts.Font
+import com.example.closenest.R
 
 val provider = GoogleFont.Provider(
     providerAuthority = "com.google.android.gms.fonts",
